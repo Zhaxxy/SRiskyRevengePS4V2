2d5731a19b131d239b80daa56db5f1d1808b8266
Zhaxxy <67915404+Zhaxxy@users.noreply.github.com>
2023-05-18 19:06:40 +0100

    Add files via upload

--- areas_to_tp_to.py
+++ areas_to_tp_to.py
@@ -3,3 +3,9 @@ from collections import namedtuple
 CoordsLayerAndArea = namedtuple('CoordsLayerAndArea',('x_coord','y_coord','area_id','current_layer'))
 
 FIRST_BOSS = CoordsLayerAndArea(2367487, 2949119, 213, 0)
+FINAL_CHECKPOINT = CoordsLayerAndArea(21381120, 18415615, 188, 2)
+RISKY_SHIP_PRE_FINAL_BOSS = CoordsLayerAndArea(1867776, 3145727, 190, 0)
+FINAL_BOSS = CoordsLayerAndArea(2277376, 2031615, 191, 0)
+
+FINAL_BOSS_BEATEN = CoordsLayerAndArea(1474560, 3145727, 192, 0)
+FINAL_CUTSCENE = CoordsLayerAndArea(3342336, 18677759, 215, 1)
